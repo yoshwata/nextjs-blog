dedd88efabed86174bb184b18d2ee1ae43fbd076
yoshwata <yoshwata@yahoo-corp.jp>
2022-01-15 04:10:08 +0000

    study

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import Layout from '../../components/layout'
+import Dashboard from '../../components/dashboard'
+import Button from '@mui/material/Button';
 
 export default function FirstPost() {
   return (
@@ -10,10 +12,14 @@ export default function FirstPost() {
       </Head>
       <h1>First Post</h1>
       <h2>
+        <Button variant="contained">Hello World</Button>;
         <Link href="/">
           <a>Back to home</a>
         </Link>
       </h2>
+      <Dashboard title="hoge">
+        fooo
+      </Dashboard>
     </Layout>
   )
 }
